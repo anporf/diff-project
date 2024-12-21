f1d7f7e651cd97e3f061f9e328ece1ec4a498c7b
anporf <anporf93@gmail.com>
2024-12-21 09:52:17 +0300

    add some functions

--- lib/utils.py
+++ lib/utils.py
@@ -1,4 +1,5 @@
 import torch
+import math
 
 
 def vp_edm_denoiser(x_t, t, model, alphas_cumprod, class_labels=None):
@@ -16,3 +17,62 @@ def vp_edm_denoiser(x_t, t, model, alphas_cumprod, class_labels=None):
 def get_sampling_timesteps(num_steps, schedule_fn, device='cuda'):
     t_vals = torch.linspace(1., 0., steps=num_steps + 1, device=device)
     return t_vals
+
+
+def broadcast_to(
+        arr, x,
+        dtype=None, device=None, ndim=None):
+    if x is not None:
+        dtype = dtype or x.dtype
+        device = device or x.device
+        ndim = ndim or x.ndim
+    out = torch.as_tensor(arr, dtype=dtype, device=device)
+    return out.reshape((-1,) + (1,) * (ndim - 1))
+
+
+def get_logsnr_schedule(
+        schedule,
+        logsnr_min: float = -20.,
+        logsnr_max: float = 20.,
+        rescale: bool = False,
+):
+    logsnr_min, logsnr_max = torch.as_tensor(logsnr_min, dtype=torch.float64), \
+        torch.as_tensor(logsnr_max, dtype=torch.float64)
+    if schedule == "linear":
+        def logsnr2t(logsnr):
+            return torch.sigmoid(logsnr)
+
+        def t2logsnr(t):
+            return torch.logit(t)
+    elif schedule == "sigmoid":
+        logsnr_range = logsnr_max - logsnr_min
+
+        def logsnr2t(logsnr):
+            return (logsnr_max - logsnr) / logsnr_range
+
+        def t2logsnr(t):
+            return logsnr_max - t * logsnr_range
+    elif schedule == "cosine":
+        def logsnr2t(logsnr):
+            return torch.atan(torch.exp(-0.5 * logsnr)) / (0.5 * torch.pi)
+
+        def t2logsnr(t):
+            return -2 * torch.log(torch.tan(t * torch.pi * 0.5))
+
+        rescale = 2 / math.pi if rescale else 1.0
+    else:
+        raise NotImplementedError("Unknown schedule")
+
+    t_from = logsnr2t(logsnr_max)
+    t_to = logsnr2t(logsnr_min)
+
+    def schedule_fn(t):
+        _t = t.to(torch.float64)
+        _t_from, _t_to = broadcast_to(t_from, _t), broadcast_to(t_to, _t)
+        logsnr = t2logsnr(torch.lerp(_t_from, _t_to, _t))
+        if rescale:
+            _rescale = rescale if isinstance(rescale, float) else 1.0
+            t.copy_(logsnr2t(logsnr).to(t.dtype) * _rescale)
+        return logsnr.to(t.dtype)
+
+    return schedule_fn
--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from lib.plot import visualize_samples
 import pickle
 from edm.dnnlib.util import open_url
 import json
-import lib.solvers.euler as euler
+import lib
+from lib.utils import get_logsnr_schedule
 
 
 
@@ -21,20 +22,46 @@ def parse_args():
         help="Specify the type of solver to use (e.g., euler, ddim)"
     )
     parser.add_argument('--download-dataset', action='store_true')
-    parser.add_argument('--calculate-fid', action='store_true')
     return parser.parse_args()
 
 
 def sample_euler(params, model, noise, class_labels, is_logUniform=False):
     num_steps = params['num_stemps']
     if is_logUniform:
-        betas = euler.get_beta_schedule_logUniform(num_steps)
+        betas = lib.euler.get_beta_schedule_logUniform(num_steps)
     else:
-        betas = euler.get_beta_schedule(num_steps)
+        betas = lib.euler.get_beta_schedule(num_steps)
     alphas = 1.0 - betas
     alphas_cumprod = np.cumprod(alphas)
-    x_euler, _ = euler.sample_euler_vp(model, noise, betas, alphas_cumprod, class_labels=class_labels, **params)
+    x_euler, _ = lib.euler.sample_euler_vp(model, noise, betas, alphas_cumprod, class_labels=class_labels, **params)
     visualize_samples('Euler Method', x_euler)
+
+
+def sample_ddim(params, model, noise, class_labels, is_logUniform=False):
+    betas = lib.euler.get_beta_schedule(params['num_stemps']_steps)
+    alphas = 1.0 - betas
+    alphas_cumprod = np.cumprod(alphas)
+
+    x_ddim, _ = lib.ddim.sample_ddim(model, noise, alphas_cumprod, class_labels=class_labels, **params)
+    visualize_samples('DDIM Method', x_ddim)
+
+
+def sample_ddim_logSNR(params, model, noise, class_labels, is_logUniform=False):
+    schedule_fn = get_logsnr_schedule(schedule='sigmoid', logsnr_min=-20., logsnr_max=20.)
+
+    x_ddim_logsnr, _ = lib.ddim_logSNR.sample_ddim_logsnr(
+        model, noise, schedule_fn, num_steps=300, class_labels=class_labels, **params
+    )
+    visualize_samples('DDIM Method with Log-SNR Schedule', x_ddim_logsnr)
+
+
+def sample_dpm_logSNR(params, model, noise, class_labels, is_logUniform=False):
+    schedule_fn = get_logsnr_schedule(schedule='linear', logsnr_min=-20., logsnr_max=20.)
+
+    x_dpm_solver_logsnr, _ = lib.dpm_logSNR.sample_dpm_solver_logsnr(
+        model, noise, schedule_fn, num_steps=200, class_labels=class_labels, **params
+    )
+    visualize_samples('DPM-Solver Method with Log-SNR Schedule', x_dpm_solver_logsnr)
 
 
 def main():
@@ -60,11 +87,12 @@ def main():
         sample_euler(params, model, noise, class_labels)
     elif args.solvqer_type == 'euler-logUniform':
         sample_euler(params, model, noise, class_labels, True)
-    # elif args.solvqer_type == 'euler':
-    #     sample_euler(params, model, noise, class_labels)
-    # elif args.solvqer_type == 'euler-logUniform':
-    #     sample_euler(params, model, noise, class_labels, True)
-
+    elif args.solvqer_type == 'ddim':
+        sample_euler(params, model, noise, class_labels)
+    elif args.solvqer_type == 'ddim-logSNR':
+        sample_euler(params, model, noise, class_labels)
+    else:
+        sample_euler(params, model, noise, class_labels)
 
 
 if __name__ == '__main__':
